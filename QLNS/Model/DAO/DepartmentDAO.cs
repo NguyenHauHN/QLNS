04b058b1b6ec16d235add4362bda0bbc16be05f1
dattencungkho <tranhuong24081996@gmail.com>
2017-05-17 10:41:02 +0700

    added employee manage

---
+++ QLNS/Model/DAO/DepartmentDAO.cs
@@ -0,0 +1,124 @@
+﻿using Model.EF;
+using Model.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList;
+
+namespace Model.DAO
+{
+    public class DepartmentDAO
+    {
+        QLNSDbContext db = null;
+        public DepartmentDAO()
+        {
+            db = new QLNSDbContext();
+        }
+
+        public Department GetByName(string name)
+        {
+            return db.Departments.SingleOrDefault(x => x.Name == name);
+        }
+
+        public Department GetByID(long? ID)
+        {
+            return db.Departments.Find(ID);
+        }
+
+        public string GetMaxCode()
+        {
+            var maxIDDepartment = db.Departments.OrderByDescending(x => x.Code).FirstOrDefault();
+            return maxIDDepartment.Code;
+        }
+
+        public IEnumerable<DepartmentViewModel> ListAllDepartment(string keyword, ref int totalDepartment, int page = 1, int pageSize = 5)
+        {
+
+            IQueryable<DepartmentViewModel> listDepartment = null;
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                listDepartment = from d in db.Departments
+                                 join e in db.Employees on d.ID equals e.DepartmentID into g
+                                 where d.Name.Contains(keyword) || d.Code.Contains(keyword)
+                                 select new DepartmentViewModel() { DepartmentData = d, AmountEmployee = g.Count() };
+            }
+            else
+            {
+                listDepartment = from d in db.Departments
+                                 join e in db.Employees on d.ID equals e.DepartmentID into g
+                                 select new DepartmentViewModel() { DepartmentData = d, AmountEmployee = g.Count() };
+            }
+
+            totalDepartment = listDepartment.Count();
+            return listDepartment.OrderByDescending(x => x.DepartmentData.CreateDate).ToPagedList(page, pageSize);
+
+        }
+
+
+        public DepartmentViewModel GetDepartmentByID(long ID)
+        {
+            var listDepartment = from d in db.Departments
+                                 join e in db.Employees on d.ID equals e.DepartmentID into g
+                                 select new DepartmentViewModel() { DepartmentData = d, AmountEmployee = g.Count() };
+            return listDepartment.Where(x => x.DepartmentData.ID == ID).FirstOrDefault();
+        }
+        public List<Department> ListAll()
+        {
+            return db.Departments.Where(x => x.Status == 1).ToList();
+        }
+
+        public int Insert(Department department)
+        {
+            var depar = db.Departments.Where(x => x.Code == department.Code).FirstOrDefault();
+            if (depar != null)
+            {
+                return -1;
+            }
+            else
+            {
+                //department.Status = 1;
+                department.CreateDate = DateTime.Now;
+                db.Departments.Add(department);
+                db.SaveChanges();
+                return 1;
+            }
+        }
+
+        public int Update(Department department)
+        {
+            try
+            {
+                var departmentTemp = db.Departments.Find(department.ID);
+                departmentTemp.Name = department.Name;
+                departmentTemp.Code = department.Code;
+                departmentTemp.Address = department.Address;
+                departmentTemp.Phone = department.Phone;
+                departmentTemp.Status = department.Status;
+                db.SaveChanges();
+                return 1;
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+
+        public int Delete(int ID)
+        {
+            try
+            {
+                var department = db.Departments.Find(ID);
+                db.Departments.Remove(department);
+                db.SaveChanges();
+                return 1;
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+
+        }
+    }
+}
---
+++ QLNS/QLNS/Controllers/EmployeeController.cs
@@ -0,0 +1,118 @@
+﻿using Model.DAO;
+using Model.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLNS.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index(string keyword, int page = 1, int pageSize = 10)
+        {
+            int totalEmployee = 0;
+            var listEmployee = new EmployeeDAO().ListAllEmployee(keyword, ref totalEmployee, page, pageSize);
+            ViewBag.Keyword = keyword;
+            ViewBag.Page = page;
+            return View(listEmployee);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var listDepartment = new DepartmentDAO().ListAll();
+            ViewBag.ListDepartment = listDepartment;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeViewModel employeeViewModel, string srcAvatar)
+        {
+            if (ModelState.IsValid)
+            {
+                var maxCode = new EmployeeDAO().GetMaxCode().Substring(2);
+                employeeViewModel.EmployeeData.Avatar = srcAvatar;
+                employeeViewModel.EmployeeData.Code = "NV" + (Convert.ToInt32(maxCode) + 1).ToString();
+                var result = new EmployeeDAO().Insert(employeeViewModel.EmployeeData);
+                if (result == 1)
+                {
+                    return RedirectToAction("Index", "Employee");
+                }
+                else if (result == -1)
+                {
+                    TempData["CreateErr"] = "Thêm mới không thành công!";
+                }
+            }
+            return RedirectToAction("Create", "Employee");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(long id)
+        {
+
+            var listDepartment = new DepartmentDAO().ListAll();
+            ViewBag.ListDepartment = listDepartment;
+            var employee = new EmployeeDAO().GetEmployeeByID(id);
+
+            return View(employee);
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeViewModel employee, string saveIDEmployee, string srcAvatar)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!string.IsNullOrEmpty(srcAvatar))
+                {
+                    employee.EmployeeData.Avatar = srcAvatar;
+                }
+                employee.EmployeeData.ID = Convert.ToInt32(saveIDEmployee);
+                var result = new EmployeeDAO().Update(employee.EmployeeData);
+                if (result == 1)
+                {
+                    return RedirectToAction("Index", "Employee");
+
+                }
+                else
+                {
+                    TempData["CreateErr"] = "Đã xảy ra lỗi, Cập nhật không thành công!";
+                }
+            }
+            return RedirectToAction("Edit", "Employee");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int ID)
+        {
+            var result = new EmployeeDAO().Delete(ID);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadImage()
+        {
+            try
+            {
+                var path = "";
+                if (System.Web.HttpContext.Current.Request.Files.AllKeys.Any())
+                {
+                    var pic = System.Web.HttpContext.Current.Request.Files["HelpSectionImages"];
+                    HttpPostedFileBase filebase = new HttpPostedFileWrapper(pic);
+                    var fileName = Path.GetFileName(filebase.FileName);
+                    path = Path.Combine(Server.MapPath("~/Images/"), fileName);
+                    filebase.SaveAs(path);
+                    return Json(new { name = "/Images/" + fileName });
+                }
+                else { return Json(new { name = "/Images/plus.png" }); }
+            }
+            catch (Exception ex)
+            {
+                return Json("Error While Saving.");
+            }
+        }
+    }
+}
